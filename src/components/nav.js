7ed0fa701de8b98ca7a968943b4319f66ef10e24
Atik Mahbub <amahbub@think-bricks.com>
2020-04-20 14:23:38 +0600

    logo added,navbar modified

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,43 +3,9 @@ import SkewLoader from "react-spinners/SkewLoader";
 import Brightness4Icon from '@material-ui/icons/Brightness4';
 import Brightness7Icon from '@material-ui/icons/Brightness7';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import corona from '../image/corona.png'
+import '../css/nav.css'
 
-
-const style = {
-
-    navbar : {
-    background : "#333",
-    height : "8vh",
-    position : "fixed",
-    top : "0",
-    color : "white",
-    width : "100%",
-    display : "flex", 
-    justifyContent : "space-between",
-    alignItems : "center",
-    zIndex: "1000"
-    },
-    text :{
-        fontSize : "1.2rem",
-        fontWeight : "900",
-        textTransform : "uppercase",
-        paddingLeft : "15px",
-        display:"flex",
-        
-    },
-    text2 : {
-        paddingRight : "10px",
-        display : "flex",
-    },
-    loader :{
-        paddingLeft : "20px"
-    },
-    toggle :{
-        marginRight : "20px",
-        cursor : "pointer"
-    }
-
-}
 
 function Nav({themeColorCallback}) {
     const [theme , setTheme] = React.useState(localStorage.getItem('theme-color') || 'light')
@@ -51,17 +17,20 @@ function Nav({themeColorCallback}) {
     themeColorCallback(theme)
 
     return (
-        <div  style={style.navbar} className="nav">
-            <div style={style.text}>Covid-19 <span className="live-tage">
+        <div className={theme === "dark" ? "navbar navbar-dark" : "navbar"}>
+            <div className = "nav-text">
+            <img src={corona} alt="logo" width="25" height ="25" className= "corona-image"/>
+                Covid-19 
+                <span className="live-tage">
                 <SkewLoader
                 size = {11}
                 color="red"
-                
+                className="nav-loader"
                 />
                 </span></div>
             
-            <div style={style.text2} data-toggle="tooltip" data-placement="top" title="toogle light/dark theme">
-                <div style={style.toggle}>
+            <div className="nav-text2" data-toggle="tooltip" data-placement="top" title="toogle light/dark theme">
+                <div className="nav-toggle">
                     {
                         theme === 'light' ? <Brightness4Icon onClick={() => handleTheme('dark')}/> : <Brightness7Icon onClick={() => handleTheme('light')}/>
                     }
